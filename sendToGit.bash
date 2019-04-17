cfd083ff78d4400a049dfed7ae9e6da98630114b
Clay Brooks <clay_brooks@outlook.com>
2019-04-17 00:44:48 -0400

    Adding results from clay-kvm

---
+++ sendToGit.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ -z "$1" ]] ; then
+	exit
+fi
+
+git config --global user.name "Clay Brooks"
+git config --global user.email "clay_brooks@outlook.com"
+
+zip -r $USER.zip $1
+
+git add $USER.zip
+git commit -m "Adding results from ${USER}"
+git push
+
