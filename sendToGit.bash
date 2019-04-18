af48f26790cb847a0a2cbf03732528a5c3be299e
Clay Brooks <clay_brooks@outlook.com>
2019-04-17 20:35:16 -0400

    fixed location of result zips

--- sendToGit.bash
+++ sendToGit.bash
@@ -8,7 +8,7 @@ git config credential.helper store
 git config --global user.name "Clay Brooks"
 git config --global user.email "clay_brooks@outlook.com"
 
-zip -r $USER.zip $1
+zip -r results/$USER.zip $1
 
 git add $USER.zip
 git commit -m "Adding results from ${USER}"
