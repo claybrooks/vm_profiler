38009ec84b2db3380f613ec1dce56b296389e977
Clay Brooks <clay_brooks@outlook.com>
2019-04-17 20:12:09 -0400

    fixed bugs in script

--- runAllTests.bash
+++ runAllTests.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-$paraFile=/proc/sys/kernel/perf_event_paranoid
+paraFile=/proc/sys/kernel/perf_event_paranoid
 
 # store users current paranoid level
-$paranoidLevel="$(sudo cat ${paraFile})"
+paranoidLevel="$(sudo cat ${paraFile})"
 
 # set paranoid level for performance counter
 sudo sh -c "echo 1 >${paraFile}"
 
-outputFolder=!/Desktop/vm_results
+outputFolder=~/Desktop/vm_results
 sudo ./src/RunTests.py -o $outputFolder -c -r --pmult 2 -t 3 -a
 
 sudo chown -R $USER $outputFolder
