87016cc84fa6c62522b064384f61423d0a7fa18a
Clay Brooks <clay_brooks@outlook.com>
2019-04-18 22:11:49 -0400

    added ./setup to run

--- runAllTests.bash
+++ runAllTests.bash
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+curDir=$(dirname "$0")
+
 # update ourselves
 git pull
+
+# set ourselves up if necessary
+./$curDir/setup.bash
 
 paraFile=/proc/sys/kernel/perf_event_paranoid
 
@@ -11,7 +16,6 @@ paranoidLevel="$(sudo cat ${paraFile})"
 # set paranoid level for performance counter
 sudo sh -c "echo 1 >${paraFile}"
 
-curDir=$(dirname "$0")
 outputFolder=~/Desktop/vm_results
 sudo ./$curDir/src/RunTests.py -o $outputFolder -c -r --pmult 2 -t 3 -a
 
