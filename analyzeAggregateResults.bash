8a710e8992cb45a166eb11782ac2c3b7921bec99
Clay Brooks <clay_brooks@outlook.com>
2019-04-20 14:45:52 -0400

    updated scripts

--- analyzeAggregateResults.bash
+++ analyzeAggregateResults.bash
@@ -1,6 +1,15 @@
-
 #!/bin/bash
 
 outputFolder=~/Desktop/vm_results
 
 sudo ./src/RunTests.py -o $outputFolder -a --vm --vmresults results -c
+
+mkdir -p results
+prior=$PWD
+resultsFolder=~/Desktop/git/vm_profiler/results/
+
+zip -r -FS aggregateData.zip ~/Desktop/vm_results/aggregateData/*
+
+git add aggregateData.zip
+git commit -m "Adding aggregate results"
+git push
