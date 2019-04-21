da62dffe89a49d01fa38b1eee6026ec9b5e81c5a
Clay Brooks <clay_brooks@outlook.com>
2019-04-21 09:04:06 -0400

    removed folders to ensure tests are running correct

---
+++ kvmClone.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+virt-clone --original ubuntu18.10_ --name ubuntu18.10-4 --auto-clone
+virt-clone --original ubuntu18.10_ --name ubuntu18.10-5 --auto-clone
+virt-clone --original ubuntu18.10_ --name ubuntu18.10-6 --auto-clone
+virt-clone --original ubuntu18.10_ --name ubuntu18.10-7 --auto-clone
+virt-clone --original ubuntu18.10_ --name ubuntu18.10-8 --auto-clone
+virt-clone --original ubuntu18.10_ --name ubuntu18.10-9 --auto-clone
+virt-clone --original ubuntu18.10_ --name ubuntu18.10-10 --auto-clone
+virt-clone --original ubuntu18.10_ --name ubuntu18.10-11 --auto-clone
+virt-clone --original ubuntu18.10_ --name ubuntu18.10-12 --auto-clone
