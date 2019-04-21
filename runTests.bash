535577999115e68b745f5f52c41ab3b10a243485
Adam Sittler <sittleram@gmail.com>
2019-04-20 20:29:35 -0400

    updated suites

--- runTests.bash
+++ runTests.bash
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 echo Running Profiler Disk
-sudo phoronix-test-suite batch-run vm_profilerDisk
+phoronix-test-suite batch-run vm_profilerDisk
 
-echo Running Profiler Graphics
-sudo phoronix-test-suite batch-run vm_profilerGraphics
+#echo Running Profiler Graphics
+#phoronix-test-suite batch-run vm_profilerGraphics
 
 echo Running Profiler Memory
-sudo phoronix-test-suite batch-run vm_profilerMemory
+phoronix-test-suite batch-run vm_profilerMemory
 
 echo Running Profiler Processor
-sudo phoronix-test-suite batch-run vm_profilerProcessor
+phoronix-test-suite batch-run vm_profilerProcessor
 
 echo Running Profiler System
-sudo phoronix-test-suite batch-run vm_profilerSystem
+phoronix-test-suite batch-run vm_profilerSystem
