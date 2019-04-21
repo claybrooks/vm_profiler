92cdabeac06f3e43c28f62792f31aaf8c9a8d0ca
Clay Brooks <clay_brooks@outlook.com>
2019-04-21 08:55:14 -0400

    added vm arg to script

--- kickOffMultiVMTest.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-command="cd ~/Desktop/git/vm_profiler; git pull; ./kickOffMultiVMTest.bash"
-push="cd ~/Desktop/git/vm_profiler; git pull; ./sendToGit.bash ~/Desktop/vm_results"
-
-# run the commands
-ssh clay-kvm@192.168.122.151 -d -m $command
-ssh clay-kvm@192.168.122.157 -d -m $command
-ssh -t clay-kvm@192.168.122.230 -d -m $command
--- runMultiVMTests.bash
+++ runMultiVMTests.bash
@@ -11,7 +11,7 @@ paranoidLevel="$(sudo cat ${paraFile})"
 # set paranoid level for performance counter
 sudo sh -c "echo 1 >${paraFile}"
 
-outputFolder=~/Desktop/git/vm_profiler/results
+outputFolder=~/Desktop/git/vm_profiler/results/$2
 sudo ./src/RunTests.py -o $outputFolder -c -r -p 1 -t 1 -a --multiVM $1
 
 #sudo chown -R $USER $outputFolder
