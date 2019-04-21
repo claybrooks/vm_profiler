cd7132dc47636803d728407a927eddcc37213e92
Adam Sittler <sittleram@gmail.com>
2019-04-20 21:02:43 -0400

    removed packages

--- installSuite.bash
+++ installSuite.bash
@@ -5,7 +5,7 @@ sudo cp -r packages/vm_* ~/.phoronix-test-suite/test-suites/local
 
 echo Installing Dependancies
 phoronix-test-suite install vm_profilerDisk
-phoronix-test-suite install vm_profilerGraphics
+#phoronix-test-suite install vm_profilerGraphics
 phoronix-test-suite install vm_profilerMemory
 phoronix-test-suite install vm_profilerProcessor
 phoronix-test-suite install vm_profilerSystem
