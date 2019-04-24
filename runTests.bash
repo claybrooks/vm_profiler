bd9b55749aece08b0f401df3c253e25a37f416dc
Clay Brooks <clay_brooks@outlook.com>
2019-04-23 22:53:39 -0400

    included results

--- runTests.bash
+++ runTests.bash
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo Run \$phoronix-test-suite batch-setup first!
+echo with Y y n n n Y
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 echo Running Profiler Disk
 phoronix-test-suite batch-run vm_profilerDisk
-
-#echo Running Profiler Graphics
-#phoronix-test-suite batch-run vm_profilerGraphics
 
 echo Running Profiler Memory
 phoronix-test-suite batch-run vm_profilerMemory
